adf6698c00b6f0ac9f97cdee806adf7e90f2675d
Gafar Amuda <47370297+gafar123@users.noreply.github.com>
2022-09-18 23:15:00 -0500

    wip

--- server/server.go
+++ server/server.go
@@ -2,24 +2,44 @@ package main
 
 import (
 	"fmt"
+
+	"tradeout-server/models"
+	"tradeout-server/routes"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
 )
 
-var MySQLClient = mysqlDBConnect()
+const userkey = "user"
+
+var secret = []byte("secret")
 
 func main() {
+	models.MysqlDBConnect()
 	router := gin.Default()
+	store := cookie.NewStore(secret)
+	router.Use(sessions.Sessions("mysession", store))
 
-	router.POST("/register", registerAccount)
-	//router.POST("/login", login)
-
+	router.POST("/register", routes.RegisterAccount)
+	router.POST("/login", routes.Login)
+	// Private group, require authentication to access
+	private := router.Group("/private")
+	private.Use(routes.AuthRequired)
+	{
+		private.GET("/me", routes.Me)
+		private.GET("/status", routes.Status)
+	}
 	router.GET("/", func(c *gin.Context) {
+		session := sessions.Default(c)
+		message := "Bad"
+		if session.Get("ID") != nil {
+			message = session.Get("name").(string)
+		}
 		c.JSON(200, gin.H{
-			"message": "f",
+			"message": message,
 		})
 	})
-
-	router.POST("/login",login)
 
 	fmt.Printf("Starting server at port \n")
 	router.Run()
