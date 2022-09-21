d2dc81e15d77e4fc3df205ce64909946a264dfd6
HeesukJin <owen.heesuk@gmail.com>
2022-09-21 02:24:18 -0500

    sturf

---
+++ nextjs/client/pages/index.js
@@ -0,0 +1,43 @@
+import {useRef} from 'react'
+
+function HomePage() {
+    const username = useRef()
+    const password = useRef()
+
+    
+
+    const register = () => {
+        const formData = new FormData()
+        formData.append('username', username.current.value)
+        formData.append('password', password.current.value)
+        
+        fetch('http://localhost:5000/register', {method: 'POST', body: formData})
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err.message));
+    }
+
+    const login = () => {
+        fetch('http://localhost:5000/')
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+
+    const logout = () => {
+        
+    }
+
+    return (
+    <>
+        <div>Welcome to Next.js!</div>
+        <input ref={username}></input>
+        <input ref={password}></input>
+        <button onClick={register}>register</button>
+        <button onClick={login}>login</button>
+        <button onClick={logout}>logout</button>
+    </>
+    
+        )
+  }
+  
+  export default HomePage
